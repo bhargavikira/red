85e7d06fc86533f6f2dd4dd8222c2e5437b2f558
bhargavikira <pandillabhargavi59@gmail.com>
2023-12-13 23:34:50 -0600

    mycode

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,46 @@
-// nav menu style
-var nav = $("#navbarSupportedContent");
-var btn = $(".custom_menu-btn");
-btn.click
-btn.click(function (e) {
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
 
-    e.preventDefault();
-    nav.toggleClass("lg_nav-toggle");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style")
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
 });
 
 
-function getCurrentYear() {
-    var d = new Date();
-    var currentYear = d.getFullYear()
 
-    $("#displayDate").html(currentYear);
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
-
-getCurrentYear();
